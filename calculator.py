ceda5d69d145903c8308c7048fad4c42c124b835
Akshitha <24mx353@psgtech.ac.in>
2025-08-01 13:32:23 +0530

    Add division feature with divide()

--- calculator.py
+++ calculator.py
@@ -1,8 +1,16 @@
-# calculator.py
 def add(a, b):
     return a + b
+
+def subtract(a, b):
+    return a - b
 
 if __name__ == "__main__":
     x = int(input("Enter first number: "))
     y = int(input("Enter second number: "))
     print("Sum:", add(x, y))
+    print("Difference:", subtract(x, y))
+    print("Quotient:", divide(x,y))
+def divide(a, b):
+    if b == 0:
+        return "Cannot divide by zero!"
+    return a / b
